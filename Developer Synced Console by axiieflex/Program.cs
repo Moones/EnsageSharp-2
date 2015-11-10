d3bff2410f1884eaa234bf4fddc8dc83c07e8952
axiieflex <admin@axiieflex.name>
2015-11-10 05:18:43 +0300

    1

--- Developer Synced Console by axiieflex/Program.cs
+++ Developer Synced Console by axiieflex/Program.cs
@@ -1,15 +1,181 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Collections;
+using System.Runtime.InteropServices;
+using System.Runtime;
 using System.Text;
-using System.Threading.Tasks;
+using Ensage;
 
-namespace Developer_Synced_Console_by_axiieflex
+namespace axiieflex.ensage2.DeveloperSyncedConsole
 {
     class Program
     {
+
+        #region Native API import
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool ShowWindow(IntPtr hWnd, ShowWindowCommands nCmdShow);
+
+        enum ShowWindowCommands { SW_HIDE = 0x0, SW_SHOW = 0x5 }
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool IsWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool IsWindowVisible(IntPtr hWnd);
+
+        #endregion
+
+        #region Core
+        /// <summary>
+        /// Прячет окно по идентификатору
+        /// </summary>
+        /// <param name="hwnd">Идентификато окна</param>
+        /// <returns></returns>
+        static bool HideWindow(IntPtr hwnd)
+        {
+            if (IsWindow(hwnd)) // обязательно проверяем существует ли еще окно перед вызовом
+            {
+                if (IsWindowVisible(hwnd))
+                {
+                    ShowWindow(hwnd, ShowWindowCommands.SW_HIDE);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Показывает окно по идентификатору
+        /// </summary>
+        /// <param name="hwnd">Идентификатор окна</param>
+        /// <returns></returns>
+        static bool ShowWindow(IntPtr hwnd)
+        {
+            if (IsWindow(hwnd)) // обязательно проверяем существует ли еще окно перед вызовом
+            {
+                if (!IsWindowVisible(hwnd))
+                {
+                    ShowWindow(hwnd, ShowWindowCommands.SW_SHOW);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Инвертирует видимость окна по идентификатору
+        /// </summary>
+        /// <param name="hwnd">Идентификатор окна</param>
+        /// <returns></returns>
+        static bool InvertWindow(IntPtr hwnd)
+        {
+            if (IsWindowVisible(hwnd))
+            {
+                return HideWindow(hwnd);
+            }
+            else
+            {
+                return ShowWindow(hwnd);
+            }
+        }
+
+        static bool InvertConsoleWindow()
+        {
+            if (Dota2_Console == null) return false;
+            return InvertWindow(Dota2_Console.Handle);
+        }
+
+        #endregion
+
+        #region global variable
+
+        static WindowInformation Dota2_Main = new WindowInformation();
+
+        static WindowInformation Dota2_Console = new WindowInformation();
+
+        #endregion
+
         static void Main(string[] args)
         {
+
+            
+            try {
+
+                List<WindowInformation> w = WindowList.GetAllWindowsExtendedInfo();
+
+                // главное окно доты
+                Dota2_Main = w.Find(x => x.Class.Contains("SDL_app") &&                            // класс главного окна доты
+                                         x.Caption.Contains("Dota 2") &&                           // и правильное имя окна :3
+                                         x.Process.MainModule.FileName.Contains("dota2.exe") &&    // процесс dota2
+                                         x.Process.MainModule.FileName.Contains("win64"));         // только для 64 битной доты
+
+                // консольное окно доты (открываемое Ensage)
+                Dota2_Console = w.Find(x => x.Class.Contains("Console") &&                         // класс окна хоста консоли
+                                            x.Caption.Contains("AppData") &&                       // содержит информацию о путе в профиле пользователя
+                                            x.Caption.StartsWith("file://") &&                     // содержит отсылку о стартовом путе
+                                            x.Process.MainModule.FileName.Contains("dota2.exe") && // процесс dota2
+                                            x.Process.MainModule.FileName.Contains("win64"));      // только для 64 битной доты
+
+
+
+            }
+            catch (Exception)
+            {
+                // если вдруг будут ошибки, хотя я не смог такого добиться
+                // то мы просто выходим из функции и все
+                return;
+            }
+                     
+            
+            // если не нашли окно с игрой, а такой бывает ОЧЕНЬ редко - то выходим
+            if (Dota2_Main == null) return;
+            // если не нашли окно с консолью, такого вообще не бывает, однако...
+            if (Dota2_Console == null) return;
+
+            // по дефолту прячем консоль
+            HideWindow(Dota2_Console.Handle);
+            
+            Ensage.Game.OnWndProc += Game_OnWndProc;
+
+            return;
         }
+
+        private static void Game_OnWndProc(WndEventArgs args)
+        {
+
+            const uint WM_CHAR = 0x102;
+
+            // если что-то пишем что игнорим
+            if (Ensage.Game.IsChatOpen)
+                return;
+
+            // да да, я хз почему, но работает только WM_CHAR, WM_KEYDOWN+WM_KEYUP не робит, ну и нахуй :D
+            // TODO: добавить чтения биндов из config.cfg
+            if ((args.Msg == WM_CHAR) && (args.WParam == '\\'))
+            {
+                InvertConsoleWindow();
+            }
+
+        }
+
+
     }
 }
---
+++ file2cs by axiieflex/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommandLine;
+using CommandLine.Text;
+
+namespace file2cs
+{
+    class Program
+    {
+
+        class Options
+        {
+
+            [Option("name", Required = true, 
+            HelpText = "Specify name of file")]
+            public string _name { get; set; }
+
+            [Option("rel", Required = false, DefaultValue=null,
+            HelpText = "Specify relative path of file")]
+            public string _rel { get; set; }
+
+            [Option('v', "visible", Required = false, DefaultValue = "public", 
+            HelpText = "Specify visible modificator private, public, protected or internal")]
+            public string _visible { get; set; }
+
+            [Option("namespace", Required = false, DefaultValue = "",
+              HelpText = "Specify namespace for output C# file")]
+            public string _namespace { get; set; }
+
+            [Option("classname", Required = false, DefaultValue = "Resource",
+            HelpText = "Specify classname for output C# file")]
+            public string _classname { get; set; }
+
+            [Option("packing", Required = false, DefaultValue = "string",
+            HelpText = "Specify packing method for C# file string or blob")]
+            public string _packing { get; set; }
+
+            [Option('o', "output", Required = false, DefaultValue = null,
+            HelpText = "Specify output filename for C# file")]
+            public string _output { get; set; }
+
+            [Option("file", Required = true, 
+            HelpText = "Specify source filename")]
+            public string _file { get; set; }
+
+            [Option('q', "quiet", Required = false, DefaultValue=true,
+            HelpText = "Ignoring all errors")]
+            public bool _quiet { get; set; }
+
+            [ParserState]
+            public IParserState LastParserState { get; set; }
+
+            [HelpOption]
+            public string GetUsage()
+            {
+                //return CommandLine.Text.HelpText.AutoBuild(this,
+                //(HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+                return "";
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// file2cs /namespace:Namespace1 /classname:Resource /rel:RelativeName /packing:string|blob ...source 
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        // Define a class to receive parsed values
+        static void Main(string[] args)
+        {
+            var options = new Options();
+            if (CommandLine.Parser.Default.ParseArguments(args, options))
+            {
+
+                string result = "";
+
+                try
+                {
+                    if (options._packing.ToUpper() == "STRING")
+                    {
+                        var data = System.IO.File.ReadAllText(options._file);
+                    }
+                    else if (options._packing.ToUpper() == "BLOB") 
+                    {
+                        var data = System.IO.File.ReadAllBytes(options._file);
+                    }
+                } catch (Exception) {
+
+                }
+
+
+
+                if (options._output == null)
+                {
+                    Console.Write(result);
+                }
+                else
+                {
+                    try {
+                        System.IO.File.WriteAllText(options._output, result);
+                    } catch(Exception ex) {
+                        if(!options._quiet) {
+                            Console.WriteLine(ex.ToString());
+                        }
+                    }
+                    
+                }
+
+            }
+        }
+    }
+}
