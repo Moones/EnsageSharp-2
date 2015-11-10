bbfcef5b9ef2a332ec9fbb15b6ee6e8e1744866b
axiieflex <admin@axiieflex.name>
2015-11-10 04:16:26 +0300

    Стартовый коммит

---
+++ Developer Synced Console by axiieflex/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Developer_Synced_Console_by_axiieflex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
